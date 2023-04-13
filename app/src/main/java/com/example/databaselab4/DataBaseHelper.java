ec47abe6a2513c5161503afb06b1e965586bb780
ESRAA <esraaeb965@gmail.com>
2023-04-13 06:41:28 +0300

    SQLite Database Tutorial for Android Studio

--- app/src/main/java/com/example/databaselab4/DataBaseHelper.java
+++ app/src/main/java/com/example/databaselab4/DataBaseHelper.java
@@ -1,25 +1,96 @@
 package com.example.databaselab4;
 
-
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteAbortException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DataBaseHelper extends SQLiteOpenHelper {
+import java.util.ArrayList;
+import java.util.List;
 
+import kotlin.contracts.Returns;
+
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+public static final String CUSTOMER_TABLE = "CUSTOMER_TABLE";
+    public static final String COLUMN_CUSTOMER_NAME = "CUSTOMER_NAME";
+    public static final String COLUMN_CUSTOMER_AGE = "CUSTOMER_AGE";
+    public static final String COLUMN_ACTIVE_CUSTOMER = "ACTIVE_CUSTOMER";
+    public static final String COLUMN_ID = "ID";
     public DataBaseHelper(@Nullable Context context) {
         super(context, "CustomerData", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
         //First time a database get accssesed
+        String createTableStatment = " Create Table "+CUSTOMER_TABLE+" ( "+COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+COLUMN_CUSTOMER_NAME +" TEXT, "+ COLUMN_CUSTOMER_AGE+" INT , "+ COLUMN_ACTIVE_CUSTOMER +" BOOL ) " ;
+        db.execSQL(createTableStatment);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 // ver. change
     }
+
+    public boolean addOne (CostumerModel customerModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CUSTOMER_NAME, customerModel.getName());
+        cv.put(COLUMN_CUSTOMER_AGE, customerModel.getAge());
+        cv.put(COLUMN_ACTIVE_CUSTOMER, customerModel.isActive());
+
+        long insert = db.insert(CUSTOMER_TABLE, null,cv);
+        if (insert== -1){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public boolean deleteOne (CostumerModel costumerModel){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM "+ CUSTOMER_TABLE+" WHERE "+COLUMN_ID+" = "+ costumerModel.getId();
+       Cursor cursor = db.rawQuery(queryString,null);
+
+       if (cursor.moveToFirst()){
+           return true;
+       }else {
+           return false;
+       }
+
+    }
+
+    public List<CostumerModel> getEveryone(){
+        List<CostumerModel> returnList = new ArrayList<>();
+        String queryString = " SELECT * FROM "+CUSTOMER_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+        if (cursor.moveToFirst()){
+          do {
+             int customerID = cursor.getInt(0);
+             String customerName  = cursor.getString(1);
+             int customerAge = cursor.getInt(2);
+             boolean customerActive = cursor.getInt(3)== 1 ? true:false;
+
+             CostumerModel newCustomer = new CostumerModel(customerID,customerName,customerAge,customerActive);
+             returnList.add(newCustomer);
+
+          }while (cursor.moveToNext());
+        }else {
+          //fail
+        }
+        cursor.close();
+        db.close();
+
+        return returnList;
+    }
+
+
+
 }
--- app/src/main/java/com/example/databaselab4/MainActivity.java
+++ app/src/main/java/com/example/databaselab4/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.databaselab4;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
     //refrence to bottun
     Button button , button2 ;
     EditText editTextTextPersonName2 , editTextNumber2;
     Switch switch1;
     ListView listview1;
-
+    ArrayAdapter customerArrayAdapter;
+    DataBaseHelper dataBaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,27 +33,55 @@ public class MainActivity extends AppCompatActivity {
         switch1 = findViewById(R.id.switch1);
         listview1 = findViewById(R.id.listview1);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        ShowCustomersOnListView(dataBaseHelper);
+        dataBaseHelper = new DataBaseHelper(MainActivity.this);
+
+        button2.setOnClickListener(new View.OnClickListener(){
             @Override
-            //View All
+            //add
+            CostumerModel costumerModel;
             public void onClick(View v){
                 try {
-                    CostumerModel costumerModel = new CostumerModel (-1,editTextTextPersonName2.getText(),Integer.editTextNumber2.getText().toString(), switch1.isChecked());
+                   costumerModel = new CostumerModel (-1,editTextTextPersonName2.getText(),Integer.editTextNumber2.getText().toString(), switch1.isChecked());
                     Toast.makeText(  MainActivity.this, "Add Button" , Toast.LENGTH_SHORT ).show();
                 }
                 catch(Exception e){
                     Toast.makeText(  MainActivity.this, "Error Creating A Costumer" , Toast.LENGTH_SHORT ).show();
-
+                    costumerModel = new CostumerModel(-1,"error",0,false);
                 }
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            //Add
-            public void onClick(View v){
-                Toast.makeText(  MainActivity.this, "View All Button" , Toast.LENGTH_SHORT ).show();
-            }
-        });
 
+                DataBaseHelper databasehelper = new DataBaseHelper(MainActivity.this);
+                boolean success = databasehelper.addOne(costumerModel);
+                ShowCustomersOnListView(dataBaseHelper);
+                Toast.makeText(MainActivity.this,"Success"+ success,Toast.LENGTH_SHORT).show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //view all
+            public void onClick(View v){
+
+                DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+               // List<CostumerModel> everyone = dataBaseHelper.getEveryone();
+
+                ShowCustomersOnListView(dataBaseHelper);
+
+                //Toast.makeText(  MainActivity.this, "View All Button" , Toast.LENGTH_SHORT ).show();
+            }
+        });
+        listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                CostumerModel clickCustomer = (CostumerModel) adapterView.getItemAtPosition(i);
+                dataBaseHelper.deleteOne(clickCustomer);
+                ShowCustomersOnListView(dataBaseHelper);
+                Toast.makeText(MainActivity.this, " DELETED"+ clickCustomer.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void ShowCustomersOnListView(DataBaseHelper dataBaseHelper) {
+        customerArrayAdapter = new ArrayAdapter<CostumerModel>(MainActivity.this , android.R.layout.simple_list_item_1, dataBaseHelper.getEveryone());
+        listview1.setAdapter(customerArrayAdapter);
     }
 }
